722bb87c3da8df7f975ac827b66d264049f88b08
David Tsai <david@convoy.com>
2017-06-19 14:06:51 -0700

    Convert null uuids

--- dist/uuid-buffer.js
+++ dist/uuid-buffer.js
@@ -1,6 +1,9 @@
 "use strict";
 exports.__esModule = true;
 function toBuffer(uuid) {
+    if (!uuid) {
+        return Buffer.alloc(16); // Return empty buffer
+    }
     var hexStr = uuid.replace(/-/g, '');
     if (uuid.length != 36 || hexStr.length != 32)
         throw new Error("Invalid UUID string: " + uuid);
@@ -10,6 +13,8 @@ exports.toBuffer = toBuffer;
 function toString(buffer) {
     if (buffer.length != 16)
         throw new Error("Invalid buffer length for uuid: " + buffer.length);
+    if (buffer.equals(Buffer.alloc(16)))
+        return null; // If buffer is all zeros, return null
     var str = buffer.toString('hex');
     return str.slice(0, 8) + "-" + str.slice(8, 12) + "-" + str.slice(12, 16) + "-" + str.slice(16, 20) + "-" + str.slice(20);
 }
--- uuid-buffer.ts
+++ uuid-buffer.ts
@@ -1,5 +1,8 @@
 
 export function toBuffer(uuid:string):Buffer {
+  if (!uuid) {
+    return Buffer.alloc(16); // Return empty buffer
+  }
   const hexStr = uuid.replace(/-/g, '');
   if (uuid.length != 36 || hexStr.length != 32) throw new Error(`Invalid UUID string: ${uuid}`); 
   return Buffer.from(hexStr, 'hex');
@@ -7,6 +10,7 @@ export function toBuffer(uuid:string):Buffer {
 
 export function toString(buffer:Buffer):string {
   if (buffer.length != 16) throw new Error(`Invalid buffer length for uuid: ${buffer.length}`);
+  if (buffer.equals(Buffer.alloc(16))) return null; // If buffer is all zeros, return null
   const str = buffer.toString('hex');
   return `${str.slice(0, 8)}-${str.slice(8, 12)}-${str.slice(12, 16)}-${str.slice(16, 20)}-${str.slice(20)}`;
 }
